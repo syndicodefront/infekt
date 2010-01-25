4ddf85bf6058a142749c7c23b4270676d550ee2f
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2010-01-25 18:07:13 +0000

    Added some setters+getters to CNFORenderer, the color scheme now internally defaults to black text on white background with black ascii arts on a dark blue shadow/glow.

--- trunk/src/lib/nfo_renderer.h
+++ trunk/src/lib/nfo_renderer.h
@@ -50,25 +50,40 @@ typedef struct _s_color_t
 	unsigned char R;
 	unsigned char G;
 	unsigned char B;
-	unsigned char A;
-} _S_COLOR_T;
+	unsigned char A; /* 0 = invisible, 255 = opaque */
+
+	_s_color_t() { R = G = B = A = 255; }
+	_s_color_t(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255) { R = r; G = g;  B = b; A = a; }
+
+	bool operator==(const _s_color_t &o) const { return (o.R == R && o.G == G && o.B == B && o.A == A); }
+	bool operator!=(const _s_color_t &o) const { return !(*this == o); }
+} S_COLOR_T;
+
+#define _S_COLOR(R, G, B, A) _s_color_t(R, G, B, A)
+#define _S_COLOR_RGB(R, G, B) _s_color_t(R, G, B, 0xFF)
+
+#define S_COLOR_T_CAIRO(CLR) CLR.R / 255.0, CLR.G / 255.0, CLR.B / 255.0
 
 
 class CNFORenderer
 {
 protected:
+	// settings:
 	size_t m_blockHeight, m_blockWidth;
-	// colors...
-
-	PNFOData m_nfo;
-	TwoDimVector<CRenderGridBlock> *m_gridData;	
-
-	bool m_rendered;
-	cairo_surface_t *m_imgSurface;
-	int m_padding;
+	S_COLOR_T m_backColor, m_textColor, m_artColor, m_gaussColor;
 	bool m_gaussShadow;
 	int m_gaussBlurRadius;
 
+	// NFO data:
+	PNFOData m_nfo;
+	TwoDimVector<CRenderGridBlock> *m_gridData;
+	cairo_surface_t *m_imgSurface;
+
+	// internal state data:
+	bool m_rendered;
+	int m_padding;
+
+	// internal calls:
 	bool CalculateGrid();
 	void RenderBlocks(bool a_opaqueBg, bool a_gaussStep);
 	void RenderText();
@@ -76,14 +91,39 @@ public:
 	CNFORenderer();
 	virtual ~CNFORenderer();
 
+	// mainly important methods:
 	bool AssignNFO(const PNFOData& a_nfo);
 	bool DrawToSurface(cairo_surface_t *a_surface, int dest_x, int dest_y,
 		int source_x, int source_y, int width, int height);
+	// you should not call this directly without a good reason, prefer DrawToSurface:
+	bool Render();
 
+	// return the calculated image dimensions:
 	size_t GetWidth();
 	size_t GetHeight();
 
-	bool Render();
+	// color setters & getters:
+	void SetBackColor(const S_COLOR_T& nc) { m_rendered = m_rendered && (m_backColor == nc); m_backColor = nc; }
+	void SetTextColor(const S_COLOR_T& nc) { m_rendered = m_rendered && (m_textColor == nc); m_textColor = nc; }
+	void SetArtColor(const S_COLOR_T& nc) { m_rendered = m_rendered && (m_artColor == nc); m_artColor = nc; }
+	void SetGaussColor(const S_COLOR_T& nc) { m_rendered = m_rendered && (m_gaussColor == nc); m_gaussColor = nc; }
+	S_COLOR_T GetBackColor() { return m_backColor; }
+	S_COLOR_T GetTextColor() { return m_textColor; }
+	S_COLOR_T GetArtColor() { return m_artColor; }
+	S_COLOR_T GetGaussColor() { return m_gaussColor; }
+
+	// various other setters & getters:
+	void SetEnableGaussShadow(bool nb) { m_rendered = m_rendered && (m_gaussShadow == nb); m_gaussShadow = nb; }
+	bool GetEnableGaussShadow() { return m_gaussShadow; }
+
+	void SetBlockSize(size_t a_width, size_t a_height) { m_rendered = m_rendered &&
+		a_width == m_blockWidth && a_height == m_blockHeight; m_blockWidth = a_width; m_blockHeight = a_height; }
+	size_t GetBlockWidth() { return m_blockWidth; }
+	size_t GetBlockHeight() { return m_blockHeight; }
+
+	// static color helper methods for anyone to use:
+	static bool ParseColor(const char* a_str, S_COLOR_T* ar);
+	static bool ParseColor(const wchar_t* a_str, S_COLOR_T* ar);
 };
 
 
