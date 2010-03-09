38566f5a327d46b776dd494a9ef813f50ef550f0
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2010-03-09 13:03:42 +0000

    Implemented support for multiple hyperlinks in one line. Fixes issue 6.

--- trunk/src/lib/nfo_data.h
+++ trunk/src/lib/nfo_data.h
@@ -80,7 +80,7 @@ public:
 
 	const CNFOHyperLink* GetLink(size_t a_row, size_t a_col) const;
 
-	static bool FindLink(const std::string& sLine, size_t& urLinkPos, size_t& urLinkLen,
+	static bool FindLink(const std::string& sLine, size_t& uirOffset, size_t& urLinkPos, size_t& urLinkLen,
 		std::string& srUrl, const std::string& sPrevLineLink, bool& brLinkContinued);
 	static std::string GetStrippedTextUtf8(const std::wstring& a_text);
 
