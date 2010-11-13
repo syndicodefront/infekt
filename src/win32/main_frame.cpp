2c59e1b949afb656d98f492bd982da29a2427155
Corn Flakes <flakes@users.noreply.github.com>
2010-11-13 17:39:18 +0000

    Fix for "holding down SHIFT during startup causes the saved window position to be discarded"

--- src/win32/main_frame.cpp
+++ src/win32/main_frame.cpp
@@ -61,10 +61,11 @@ void CMainFrame::PreCreate(CREATESTRUCT& cs)
 
 	if(dynamic_cast<CNFOApp*>(GetApp())->GetSettingsBackend()->OpenSectionForReading(L"Frame Settings", l_sect))
 	{
-		if(!::GetKeyState(VK_SHIFT))
+		if(!::GetAsyncKeyState(VK_SHIFT))
 		{
 			// holding down SHIFT during startup causes the
 			// saved position to be discarded
+
 			cs.x = l_sect->ReadDword(L"Left");
 			cs.y = l_sect->ReadDword(L"Top");
 			cs.cx = l_sect->ReadDword(L"Width");
