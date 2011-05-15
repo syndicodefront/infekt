0cf32aac42e12a71686f5358317a9753c74d0a12
Corn Flakes <flakes@users.noreply.github.com>
2011-05-15 20:42:18 +0000

    XML and content scraper related work.

--- src/win32-plugin-imdb/main.cpp
+++ src/win32-plugin-imdb/main.cpp
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  **/
 
-#include "imdb-plugin.h"
+#include "stdafx.h"
 
 static CImdbPlugin* s_pPlugin = NULL;
 
---
+++ src/win32-plugin-imdb/stdafx.h
@@ -0,0 +1,13 @@
+#include "imdb-plugin.h"
+
+#include <Shlwapi.h>
+
+#include <stdint.h>
+
+#include <vector>
+#include <map>
+#include <limits>
+#include <stack>
+#include <boost/shared_ptr.hpp>
+
+#include <pcre.h>
