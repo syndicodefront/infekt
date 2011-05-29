95f45902de50477d3d1a2339ab8abbb1e1e83a21
Corn Flakes <flakes@users.noreply.github.com>
2011-05-29 15:22:55 +0000

    content scraper: implemented remaining missing functionality

--- src/win32-plugin-imdb/content_scraper.cpp
+++ src/win32-plugin-imdb/content_scraper.cpp
@@ -82,7 +82,41 @@ bool CContentScraper::DoScrape(const std::string& a_content)
 		return false;
 	}
 
-	// :TODO: evaluate <vars><var>...
+	m_extractedStrings.clear();
+	m_extractedLists.clear();
+
+	std::vector<const std::string> l_requiredSymbolNames;
+
+	// evaluate <vars><var>...
+	const PXMLTag l_varsTag = m_scrapeDefs->GetChildByName("vars");
+	if(l_varsTag->GetAttribute("section") == "output")
+	{
+		for(std::vector<PXMLTag>::const_iterator itv = l_varsTag->BeginChildren(); itv != l_varsTag->EndChildren(); itv++)
+		{
+			if((*itv)->GetName() != "var")
+			{
+				continue;
+			}
+
+			const std::string l_symbolName = (*itv)->GetText();
+
+			// pre-fill maps for output/results:
+			if((*itv)->GetAttribute("list") == "true")
+			{
+				m_extractedLists[l_symbolName] = std::vector<const std::string>();
+			}
+			else
+			{
+				m_extractedStrings[l_symbolName] = "";
+			}
+
+			// collect required symbols so we can rate the results:
+			if((*itv)->GetAttribute("required") == "true")
+			{
+				l_requiredSymbolNames.push_back(l_symbolName);
+			}
+		}
+	}
 
 	// define reserved symbol for entire input:
 	l_symbols["_INPUT_"] = a_content;
@@ -251,16 +285,67 @@ bool CContentScraper::DoScrape(const std::string& a_content)
 				{
 					l_symbols[l_toSymbol] = l_temp;
 				}
+				// we can only use lists in defined output vars,
+				// not temporary local symbols:
+				else if(m_extractedLists.find(l_appendToSymbol) != m_extractedLists.end())
+				{
+					m_extractedLists[l_appendToSymbol].push_back(l_temp);
+				}
 				else
 				{
-					// :TODO: implement lists
-					l_symbols[l_appendToSymbol] += l_temp;
+					l_symbols[l_toSymbol] += " " + l_temp;
 				}
 			} while (bLoopOccurrences && _FindAndConsumeDynamicGroups(&l_regex, &l_reInput, l_groups));
 		}
 	}
 
-	return true;
+	// check that all of the "required" symbols are non-empty:
+	bool l_success = true;
+
+	for(std::vector<const std::string>::const_iterator itr = l_requiredSymbolNames.begin(); itr != l_requiredSymbolNames.end(); itr++)
+	{
+		if((l_symbols.find(*itr) == l_symbols.end() || l_symbols[*itr].empty()) && m_extractedLists[*itr].empty())
+		{
+			l_success = false;
+		}
+	}
+
+	if(l_success)
+	{
+		// copy results to member var:
+		for(std::map<const std::string, std::string>::iterator its = m_extractedStrings.begin(); its != m_extractedStrings.end(); its++)
+		{
+			its->second = l_symbols[its->first];
+		}
+	}
+
+	return l_success;
+}
+
+
+int CContentScraper::GetInt(const std::string& a_name)
+{
+	return atoi(GetString(a_name).c_str());
+}
+
+
+std::string CContentScraper::GetString(const std::string& a_name)
+{
+	return (m_extractedStrings.find(a_name) != m_extractedStrings.end() ?
+		m_extractedStrings[a_name] : "");
+}
+
+
+bool CContentScraper::GetList(const std::string& a_name, std::vector<const std::string>& ar_result)
+{
+	if(m_extractedLists.find(a_name) != m_extractedLists.end() && !m_extractedLists[a_name].empty())
+	{
+		ar_result = m_extractedLists[a_name];
+
+		return true;
+	}
+
+	return false;
 }
 
 
