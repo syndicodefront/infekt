8f248445e0095573d1368a596584955b9e978306
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2011-08-28 10:57:52 +0000

    disable warning (fine according to MSDN)

--- trunk/src/win32-plugin-imdb/stdafx.h
+++ trunk/src/win32-plugin-imdb/stdafx.h
@@ -13,4 +13,8 @@
 using std::tr1::shared_ptr;
 
 #include <pcre.h>
+#pragma warning (push)
+#pragma warning (disable: 4251)
+// disable warning about DLL import of std:: class
 #include <pcrecpp.h>
+#pragma warning (pop)
