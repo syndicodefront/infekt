69fb9f22a08e1d25a2f5e0fcf55d548b86587982
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2012-04-01 21:56:44 +0000

    saving PNGs with libpng - WIP

--- trunk/src/lib/nfo_to_png.cpp
+++ trunk/src/lib/nfo_to_png.cpp
@@ -15,6 +15,7 @@
 #include "stdafx.h"
 #include "nfo_renderer_export.h"
 #include <cairo.h>
+#include <png.h>
 
 
 CNFOToPNG::CNFOToPNG(bool a_classicMode)
@@ -31,7 +32,7 @@ bool CNFOToPNG::SavePNG(const std::wstring& a_filePath)
 		return false;
 	}
 
-	if(m_stripes.size() == 1 && GetHeight() < 32767)
+	/*if(m_stripes.size() == 1 && GetHeight() < 32767)
 	{
 		std::string l_filePath =
 	#ifdef _UNICODE
@@ -41,11 +42,86 @@ bool CNFOToPNG::SavePNG(const std::wstring& a_filePath)
 	#endif
 	
 		return (cairo_surface_write_to_png(*m_stripes[0], l_filePath.c_str()) == CAIRO_STATUS_SUCCESS);
+	}*/
+
+	return SaveWithLibpng(a_filePath);
+}
+
+
+bool CNFOToPNG::SaveWithLibpng(const std::wstring& a_filePath)
+{
+	FILE *fp;
+
+	if(_wfopen_s(&fp, a_filePath.c_str(), L"wb") != 0)
+	{
+		return false;
 	}
 
-	// now about the hard way...
+	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);//(png_voidp)user_error_ptr, user_error_fn, user_warning_fn);
 
-	return false;
+	if(!png_ptr)
+	{
+		fclose(fp);
+		return false;
+	}
+
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+
+	if(!info_ptr)
+	{
+		png_destroy_write_struct(&png_ptr, NULL);
+		fclose(fp);
+		return false;
+	}
+
+	if(setjmp(png_jmpbuf(png_ptr)))
+	{
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+		fclose(fp);
+		return false;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	png_set_IHDR(png_ptr, info_ptr, GetWidth(), GetHeight(),
+		8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+	png_bytep *row_ptr = new (std::nothrow) png_bytep[GetHeight()];
+
+	if(row_ptr)
+	{
+		size_t l_png_row = 0;
+
+		for(size_t l_stripe = 0; l_stripe < m_stripes.size(); l_stripe++)
+		{
+			unsigned char *l_data = cairo_image_surface_get_data(*m_stripes[l_stripe]);
+			size_t l_stride = cairo_image_surface_get_stride(*m_stripes[l_stripe]);
+			size_t l_num_rows = cairo_image_surface_get_height(*m_stripes[l_stripe]);
+
+			for(size_t l_row = 0; l_row < l_num_rows; l_row++)
+			{
+				row_ptr[l_png_row] = (png_bytep)(l_data[l_row * l_stride]);
+
+				l_png_row++;
+			}
+		}
+
+		png_set_rows(png_ptr, info_ptr, row_ptr);
+	}
+
+	png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+	png_write_end(png_ptr, info_ptr);
+	png_destroy_write_struct(&png_ptr, &info_ptr); 
+
+	fclose(fp);
+
+	if(row_ptr)
+	{
+		delete[] row_ptr;
+	}
+
+	return true;
 }
 
 
