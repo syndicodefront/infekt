6f935c54c157de9e3beff96bfa05554bd03be786
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2012-06-07 22:45:13 +0000

    another build/deps script update

--- trunk/build-out/copy-deps.cmd
+++ trunk/build-out/copy-deps.cmd
@@ -1,3 +1,9 @@
+REM Copy deps for running
+
+mkdir Debug
+mkdir Release
+mkdir Debug-x64
+mkdir Release-x64
 copy ..\dependencies\lib_x86_debug\*.dll Debug
 copy ..\dependencies\lib_x86_release\*.dll Release
 copy ..\dependencies\lib_x64_debug\*.dll Debug-x64
