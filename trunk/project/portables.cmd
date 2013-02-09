199556847552c6590d2adb445a72bd95825600f3
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2013-02-09 02:46:14 +0000

    further progress in Visual Studio 2012 update

--- trunk/project/portables.cmd
+++ trunk/project/portables.cmd
@@ -35,11 +35,11 @@ xcopy /Y "%ROOTDIR%\..\release\*.url" %PP64%
 
 rem COPY RUNTIME DLLS
 
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\*.dll" %PP32%
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.OpenMP\*.dll" %PP32%
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC110.CRT\msvc*.dll" %PP32%
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC110.OpenMP\*.dll" %PP32%
 
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x64\Microsoft.VC100.CRT\*.dll" %PP64%
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x64\Microsoft.VC100.OpenMP\*.dll" %PP64%
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x64\Microsoft.VC110.CRT\msvc*.dll" %PP64%
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x64\Microsoft.VC110.OpenMP\*.dll" %PP64%
 
 rem COPY BUILT FILES
 
@@ -69,8 +69,8 @@ xcopy /Y /S /E %ROOTDIR%\..\release\PortableApps %PAP%
 
 copy /Y %ROOTDIR%\..\release\portable.ini %PAP%\App\DefaultData
 
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\*.dll" %PAP%\App\iNFekt
-xcopy /Y "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.OpenMP\*.dll" %PAP%\App\iNFekt
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC110.CRT\*.dll" %PAP%\App\iNFekt
+xcopy /Y "%VS110COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC110.OpenMP\*.dll" %PAP%\App\iNFekt
 copy /Y %ROOTDIR%\Release\cuda-blur.dll %PAP%\App\iNFekt
 copy /Y %ROOTDIR%\Release\infekt-win32.exe %PAP%\App\iNFekt
 copy /Y %ROOTDIR%\Release\iNFektPortableLauncher.exe %PAP%\iNFektPortable.exe
