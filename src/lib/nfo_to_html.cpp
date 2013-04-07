b94d82278d7b5e50761cfca77f1262af389095ae
Corn Flakes <flakes@users.noreply.github.com>
2013-04-07 22:21:00 +0000

    Export HTML5 instead of XHTML.

--- src/lib/nfo_to_html.cpp
+++ src/lib/nfo_to_html.cpp
@@ -30,13 +30,13 @@ const wstring CNFOToHTML::GetHTML(bool a_includeHeaderAndFooter)
 	
 	if(a_includeHeaderAndFooter)
 	{
-		l_html << L"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" << endl;
-		l_html << L"<html xmlns=\"http://www.w3.org/1999/xhtml\">" << endl;
+		l_html << L"<!DOCTYPE html>" << endl;
+		l_html << L"<html>" << endl;
 		l_html << L"<head>" << endl;
 
 		l_html << L"  <title>" << XMLEscape(m_title) << L"</title>" << endl;
 
-		l_html << L"  <style type=\"text/css\">" << endl;
+		l_html << L"  <style>" << endl;
 		l_html << L"  body { font-family: sans-serif; margin: 0; padding: 5px 15px; background: #fff; }" << endl;
 		l_html << endl;
 
