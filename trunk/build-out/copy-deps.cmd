d73c09033f3fb8b3a1394f8d96fb3861f2eae222
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2013-10-02 17:16:01 +0000

    copy dependencies' pdbs for debugging

--- trunk/build-out/copy-deps.cmd
+++ trunk/build-out/copy-deps.cmd
@@ -5,6 +5,8 @@ mkdir Release
 mkdir Debug-x64
 mkdir Release-x64
 copy ..\dependencies\lib_x86_debug\*.dll Debug
+copy ..\dependencies\lib_x86_debug\*.pdb Debug
 copy ..\dependencies\lib_x86_release\*.dll Release
 copy ..\dependencies\lib_x64_debug\*.dll Debug-x64
+copy ..\dependencies\lib_x64_debug\*.pdb Debug-x64
 copy ..\dependencies\lib_x64_release\*.dll Release-x64
