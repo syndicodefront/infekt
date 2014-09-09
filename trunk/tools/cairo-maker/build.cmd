a1533b93536fc3b1a1c96fe7c59dcf8e799de02d
cxxjoe <cxxjoe@29bd351a-09b2-11df-bc4b-c3c7eeec796e>
2014-09-09 19:50:18 +0000

    pixman + libpng update

--- trunk/tools/cairo-maker/build.cmd
+++ trunk/tools/cairo-maker/build.cmd
@@ -21,11 +21,11 @@ curl http://zlib.net/zlib-1.2.8.tar.gz -o zlib.tgz
 :AZOK
 
 IF EXIST libpng.tgz GOTO LPZOK
-curl ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.10.tar.gz -o libpng.tgz
+curl ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.13.tar.gz -o libpng.tgz
 :LPZOK
 
 IF EXIST pixman.tgz GOTO PZOK
-curl http://www.cairographics.org/releases/pixman-0.32.4.tar.gz -o pixman.tgz
+curl http://www.cairographics.org/releases/pixman-0.32.6.tar.gz -o pixman.tgz
 :PZOK
 
 IF EXIST cairo.tar.xz GOTO CZOK
