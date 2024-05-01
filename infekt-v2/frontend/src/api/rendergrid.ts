b446f712f08b2c7b67f1f8a8eb31c056285a2a3d
flakes <cxxjoe@gmail.com>
2024-05-01 13:07:23 +0200

    2.0 - some types for renderer

---
+++ infekt-v2/frontend/src/api/rendergrid.ts
@@ -0,0 +1,44 @@
+export interface NfoRendererGrid {
+  width: number;
+  height: number;
+  lines: NfoRendererLine[];
+}
+
+export interface NfoRendererLine {
+  row: number;
+  blockGroups: NfoRendererBlockGroup[];
+  textFlights: NfoRendererTextFlight[];
+  links: NfoRendererLink[];
+}
+
+export interface NfoRendererBlockGroup {
+  col: number;
+  blocks: NfoRendererBlockShape[];
+}
+
+export enum NfoRendererBlockShape {
+  NoBlock = 0,
+  Whitespace,
+  WhitespaceInText,
+  FullBlock,
+  FullBlockLightShade,
+  FullBlockMediumShade,
+  FullBlockDarkShade,
+  LowerHalf,
+  UpperHalf,
+  RightHalf,
+  LeftHalf,
+  BlackSquare,
+  BlackSquareSmall,
+}
+
+export interface NfoRendererTextFlight {
+  col: number;
+  text: string;
+}
+
+export interface NfoRendererLink {
+  col: number;
+  text: string;
+  url: string;
+}
